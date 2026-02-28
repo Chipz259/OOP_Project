8d58c65641779a6167f1c4157547ca5c4ea638f8
Apich01 <68070202@kmitl.ac.th>
2026-02-28 14:02:16 +0700

    abb Scene การจัดการแสดงของฉาก

--- Script/Scene.java
+++ Script/Scene.java
@@ -1,41 +1,59 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+
 public class Scene {
     private String sceneId;
     private BufferedImage backgroundImage;
     private ArrayList<GameObject> objectsInScene;
-    public Scene(String sceneId, BufferedImage bg) {
 
+    public Scene(String sceneId) {
+        this.sceneId = sceneId;
+        this.objectsInScene = new ArrayList<>();
     }
+
     public String getSceneId() {
-        return "0"; //ใส่ไว้ก่อน กัน error
+        return sceneId;
     }
-    public void setSceneId(String sceneId) {
 
-    }
     public BufferedImage getBackgroundImage() {
-        return backgroundImage; //ใส่ไว้ก่อน กัน error
+        return backgroundImage;
     }
     public void setBackgroundImage(BufferedImage bg) {
-
+        this.backgroundImage = bg;
     }
     public ArrayList<GameObject> getObjectsInScene() {
-        return objectsInScene; //ใส่ไว้ก่อน กัน error
+        return objectsInScene;
     }
-    public void setObjectsInScene(ArrayList<GameObject> objectsInScene) {
 
-    }
     public void addGameObject(GameObject obj) {
-
+        if (obj != null) {
+            objectsInScene.add(obj);
+        }
     }
+
     public void removeGameObject(GameObject obj) {
-
+        if (obj != null) {
+            objectsInScene.remove(obj);
+        }
     }
+
     public void update() {
-
+        for (int i = 0; i < objectsInScene.size(); i++){
+            GameObject obj = objectsInScene.get(i);
+            obj.update();
+        }
     }
-    public void render(Graphics2D g2d) {
 
+    public void render(Graphics2D g2d) {
+        if (backgroundImage != null) {
+            g2d.drawImage(backgroundImage, 0, 0, 1920, 1080, null);
+        }
+
+        for (GameObject obj : objectsInScene){
+            if (obj.isVisible()){
+                obj.render(g2d);
+            }
+        }
     }
 }
