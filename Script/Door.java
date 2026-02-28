d192e0ec1eccdd51e103c6f4354db53378624b13
Apich01 <68070202@kmitl.ac.th>
2026-02-28 13:11:49 +0700

    add Door ปุ่มลูกศรสำหรับเปลี่ยนฉาก

--- Script/Door.java
+++ Script/Door.java
@@ -1,47 +1,76 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
 public class Door extends GameObject implements Interactable {
-    private String nextSceneId;
+
     private int spawnX;
     private int spawnY;
-    public Door(String id, int x, int y, int width, int height, String nextSceneId, int spawnX, int spawnY) {
+    private String nextSceneId;
+    private SceneManager sceneM;
+
+    public Door(String id, int x, int y, int width, int height, String nextSceneId, SceneManager sceneM, BufferedImage arrowImage) {
         super(id, x, y, width, height);
+
+        this.nextSceneId = nextSceneId;
+        this.sceneM = sceneM;
+
+        //กำหนดรูปภาพลูกศรให้ตัวแปร sprite
+        this.setSprite(arrowImage);
+
+        //กำหนดจุดเกิดของ Player
+        this.spawnX = 100;
+        this.spawnY = 500;
     }
+
+    //Setter กับ Getter
     public String getNextSceneId() {
-        return "0"; //ใส่ไว้ก่อน กัน error
+        return nextSceneId;
     }
     public void setNextSceneId(String nextSceneId) {
-
+        this.nextSceneId = nextSceneId;
     }
     public int getSpawnX() {
-        return 0; //ใส่ไว้ก่อน กัน error
+        return spawnX;
     }
     public void setSpawnX(int spawnX) {
-
+        this.spawnX = spawnX;
     }
     public int getSpawnY() {
-        return 0; //ใส่ไว้ก่อน กัน error
+        return spawnY;
     }
     public void setSpawnY(int spawnY) {
-
+        this.spawnY = spawnY;
     }
+
+
     @Override
     public void update() {
 
     }
+
     @Override
     public void render(Graphics2D g2d) {
-
+        if (getSprite() != null && isVisible()){
+            g2d.drawImage(getSprite(), this.getX(), this.getY(), this.getSpawnX(), this.getSpawnY(), null);
+        }
     }
+
     @Override
     public void onInteract(Player p) {
-
+        System.out.println("ผู้เล่นคลิกลูกศร: เปลี่ยนไปฉาก " + this.getNextSceneId());
+        if (sceneM != null) {
+            sceneM.loadScene(this.getNextSceneId());
+            p.setX(this.getSpawnX());
+            p.setY(this.getSpawnY());
+        }
     }
+
     @Override
     public void onHover() {
 
     }
+
     @Override
     public boolean isInteractable() {
-        return true; //ใส่ไว้ก่อน กัน error
+        return true;
     }
 }
