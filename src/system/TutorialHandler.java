58503828117957d256645c593ef8f1b9336ba70a
Lavendis <wongsatorn.saehao@gmail.com>
2026-03-26 12:37:26 +0700

    หน้า Tutorail เสร็จแล้ว

---
+++ src/system/TutorialHandler.java
@@ -0,0 +1,27 @@
+package system;
+
+import ui.Tutorial;
+import java.awt.event.*;
+
+public class TutorialHandler implements MouseListener {
+    private Tutorial gui;
+
+    public TutorialHandler(Tutorial gui) {
+        this.gui = gui;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {}
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+}
